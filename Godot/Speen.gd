7cab383327daf4d366f861d2e588093c78d342cd
Kerfufl <eoinj@live.ie>
2023-04-25 10:03:36 +0100

    Pod initial creation
    Added dodecahedron to scene, made child of creature and speens slowly

---
+++ Godot/Speen.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+export var speed = 5
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _process(delta):
+	#slowly rotates on x axis
+	rotate_x(.01)
+#	pass
